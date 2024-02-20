25a2288e2276fcc869b10316eea66933049ab279
LucaB98 <lucabellan98@gmail.com>
2024-02-20 17:16:35 +0100

    add background

--- script.php
+++ script.php
@@ -52,29 +52,38 @@
 <!-- bootstrap -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
+<!-- font-awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<!-- css -->
+<link rel="stylesheet" href="css/style.css" type="text/jpg">
+
 </head>
 <body>
-<div class="container mt-5">
-<h1>Tabella hotel</h1>
-    <table class="table table-striped">
-        <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Descrizione</th>
-            <th scope="col">Parking</th>
-            <th scope="col">Vote</th>
-            <th scope="col">Distance to center</th>
-        </tr>
-        <?php foreach($hotels as $key => $hotel) : ?>
-            <tr>
-                <?php foreach($hotel as $value) : ?>
-                  <td>
-                    <?= $value ?>
-                  </td> 
-                <?php endforeach ?>   
-            </tr>
-        <?php endforeach ?>
-    </table>
-</div>
+<main class="main">
+    <div class="container mt-5">
+        <h1 class="my-5">Tabella hotel</h1>
+            <table class="table table-striped">
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance to center</th>
+                </tr>
+                <?php foreach($hotels as $key => $hotel) : ?>
+                    <tr>
+                        <?php foreach($hotel as $value) : ?>
+                        <td>
+                            <?= $value ?>
+                        </td> 
+                        <?php endforeach ?>   
+                    </tr>
+                <?php endforeach ?>
+            </table>
+    </div>
+</main>
+
 
 <!-- js bootstrap -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
